3b276ce5df2fc55932e8c3b62d6551449b60f6dc
MaximilianScheel <scheel.development@gmail.com>
2023-07-25 11:55:16 +0200

    Implementing models

--- tdlbackend/settings.py
+++ tdlbackend/settings.py
@@ -25,8 +25,14 @@ SECRET_KEY = 'django-insecure-+vne4vu#eoj(w*i=u+xga)eq_pkt_)35ttg3ma1)9#5c4x&je&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',]
+    
 
 # Application definition
 
@@ -38,10 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'todolist'
+    'rest_framework.authtoken',
+    'todolist',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +131,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
