d53e58445ad3c664b26a9e7a32fff3ee8a309cac
MaximilianScheel <scheel.development@gmail.com>
2023-07-26 12:22:33 +0200

    Implementing token

--- tdlbackend/urls.py
+++ tdlbackend/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 
 from todolist.views import LoginView
+from todolist.views import TodoItemsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', LoginView.as_view()),
+    path('todos/', TodoItemsView.as_view()),
     
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import TodoItem
+from .serializers import TodoItemSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+class TodoItemsView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
-# Create your views here.
+    def get(self, request, format=None):
+        todos = TodoItem.objects.filter(author=request.user)
+        serializer = TodoItemSerializer(todos, many=True)
+        return Response(serializer.data)
 
 class LoginView(ObtainAuthToken):
-     def post(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data,
                                            context={'request': request})
         serializer.is_valid(raise_exception=True)
@@ -17,4 +29,4 @@ class LoginView(ObtainAuthToken):
             'token': token.key,
             'user_id': user.pk,
             'email': user.email
-        })
+        })  
